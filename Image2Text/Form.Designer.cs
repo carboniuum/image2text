ffdc960f772532ff2d6e527489668adb4f7e5631
Umarbek <umarbek@mail.com>
2022-04-20 10:44:22 +0500

    first commit

---
+++ Image2Text/Form.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Image2Text
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.InitialImage = null;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 449);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image2Text";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Image2Text/Form.cs
@@ -0,0 +1,102 @@
+﻿using IronOcr;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Image2Text
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Point _rectStartPoint;
+        private Rectangle _rect = new Rectangle();
+        private Brush _selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+
+        private IronTesseract _ocr = new IronTesseract(); 
+
+        public Form()
+        {
+            InitializeComponent();
+
+            _ocr.Language = OcrLanguage.EnglishBest;
+            _ocr.Configuration.TesseractVersion = TesseractVersion.Tesseract5;
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            _rectStartPoint = e.Location;
+            Invalidate();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            Point tempEndPoint = e.Location;
+
+            _rect.Location = new Point(
+                Math.Min(_rectStartPoint.X, tempEndPoint.X),
+                Math.Min(_rectStartPoint.Y, tempEndPoint.Y)
+            );
+            _rect.Size = new Size(
+                Math.Abs(_rectStartPoint.X - tempEndPoint.X),
+                Math.Abs(_rectStartPoint.Y - tempEndPoint.Y)
+            );
+
+            pictureBox.Invalidate();
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (_rect.Contains(e.Location))
+                {
+                    var source = new Bitmap(pictureBox.Image);
+                    var croppedImage = CropImage(source, _rect);
+
+                    using (var ocrInput = new OcrInput())
+                    {
+                        ocrInput.AddImage(croppedImage);
+                        var result = _ocr.Read(ocrInput);
+                        string txt = result.Text;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                if (_rect != null && _rect.Width > 0 && _rect.Height > 0)
+                {
+                    e.Graphics.FillRectangle(_selectionBrush, _rect);
+                }
+            }
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            int width = control.Size.Width;
+            int height = control.Size.Width;
+
+            pictureBox.Size = new Size(width, height);
+        }
+
+        private Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            var bitmap = new Bitmap(section.Width, section.Height);
+
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+                return bitmap;
+            }
+        }
+    }
+}
